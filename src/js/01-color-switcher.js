48f1b71c16dfdd8b66266e982997f3e1ddeb3a81
AntonTy35 <antony.tislenko@gmail.com>
2023-06-26 23:41:59 +0300

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+stopBtn.setAttribute('disabled', 'true');
+
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+let colorInterval;
+
+function onStartClick() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  colorInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopClick() {
+  clearInterval(colorInterval);
+
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
