d34888d3acac73c3e206ba62fd9a8f0ad342ea04
AntonTy35 <123330468+AntonTy35@users.noreply.github.com>
2023-06-26 20:42:34 +0000

    Deploying to gh-pages from @ AntonTy35/goit-js-hw-09@48f1b71c16dfdd8b66266e982997f3e1ddeb3a81 🚀

---
+++ 01-color-switcher.a6586715.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let d;e.setAttribute("disabled","true"),t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.a6586715.js.map
